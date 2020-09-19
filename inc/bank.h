0098da79dfc0753958cb4d0bfc82854f5445c2c8
99002638 <71481322+99002638@users.noreply.github.com>
2020-09-19 08:59:27 +0530

    Create bank.h

---
+++ inc/bank.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+createaccount(int)
+viewaccount(number);
+                
+addmoney(number);
+             
+                
+withdraw(int);
+           
+transfer(int);
+             
+mobilechange(int);
+          
+accountsearch(number);
